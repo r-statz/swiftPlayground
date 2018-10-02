f8f700fd964a32a45bfdf0f11a86b1f6a1a32ae6
rstatz <robinstatz@gmail.com>
2018-10-02 15:40:40 -0600

    second commit, some styling done.

--- xcodePlayground/xcodePlayground/ViewController.swift
+++ xcodePlayground/xcodePlayground/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Robin Statz on 10/2/18.
 //  Copyright © 2018 Robin Statz. All rights reserved.
-//
+//(red: 48, green: 72, blue: 120, alpha: 1)
 
 import UIKit
 
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let header = UIView(frame : CGRect(x:0, y:0, width:self.view.frame.width, height:self.view.frame.height * 0.08))
+        header.backgroundColor = UIColor.red
+        self.view.addSubview(header)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
